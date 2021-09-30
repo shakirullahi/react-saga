63b4bc69eded3fb6cd62c8216b513c64af3d60b0
Sakirullahi <opshaktvl@gmail.com>
2021-09-30 16:52:35 +0530

    basics 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Container, Header, Statistic } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello From Budget
-    </div>
+    <Container>
+      <Header as='h1'>Budget </Header>
+      <Statistic size='small'>
+        <Statistic.Label>Your Balance</Statistic.Label>
+        <Statistic.Value>10,000.00</Statistic.Value>
+      </Statistic>
+    </Container>
   );
 }
 
