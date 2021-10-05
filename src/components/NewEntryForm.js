3a8a6a07d2e06ce5838b1bed570a1db568ef7469
Sakirullahi <opshaktvl@gmail.com>
2021-10-05 15:43:12 +0530

    refractored isExpense toggle button

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import { Form } from 'semantic-ui-react';
+import { Checkbox, Form, Segment } from 'semantic-ui-react';
 import ButtonSaveOrCancel from './ButtonSaveOrCancel';
 
 const NewEntryForm = ({addEntry}) => {
   const [title, setTitle] = useState('');
   const [value, setValue] = useState('');
-  const [isExpense, setIsExpense] = useState(false);
+  const [isExpense, setIsExpense] = useState(true);
   const clearForm = ()=> {
     setTitle('');
     setValue('');
@@ -15,15 +15,24 @@ const NewEntryForm = ({addEntry}) => {
     return (
         <Form unstackable>
         <Form.Group>
-          <Form.Input icon='tags' width='10' label='Description' placeholder="New shiny thing" value={title} onChange={(e)=>setTitle(e.target.value)} />
+          <Form.Input icon='tags' width='12' label='Description' placeholder="New shiny thing" value={title} onChange={(e)=>setTitle(e.target.value)} />
 
           <Form.Input width='4' label='Value' placeholder='100.00' icon='dollar' iconPosition='left' value={value}  onChange={(e)=>setValue(e.target.value)} />
-
-            <Form.Checkbox width='2' label='Is Expense' checked={isExpense}  onChange={(e)=>setIsExpense(e.target.checked)} />
         </Form.Group>
 
+        <Segment compact>
+          <Checkbox 
+          label="Is Expense"
+          toggle
+          checked={isExpense}
+          onChange={()=>setIsExpense(oldState=>!oldState)}
+          />
+          </Segment>
+        
         <ButtonSaveOrCancel save='Save' cancel='Cancel' title={title} value={value} isExpense={isExpense} addEntry={addEntry} clearForm={clearForm}/>
         
+        
+
       </Form>
     )
 }
