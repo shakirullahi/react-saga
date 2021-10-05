c1225089b0588460ecbdedba6d4479cb2e864119
Sakirullahi <opshaktvl@gmail.com>
2021-10-05 14:02:32 +0530

    added function to Cancel button

--- src/components/ButtonSaveOrCancel.js
+++ src/components/ButtonSaveOrCancel.js
@@ -5,7 +5,7 @@ import { Button } from 'semantic-ui-react'
 const ButtonSaveOrCancel = ({save='OK', cancel='Cancel', title, value, addEntry,clearForm}) => {
     return (
         <Button.Group style={{marginTop:'20px'}}>
-          <Button>{cancel}</Button>
+          <Button onClick={()=>clearForm()}>{cancel}</Button>
           <Button.Or />
           <Button primary onClick={()=>{clearForm();addEntry(title,value)}}>{save}</Button>
         </Button.Group>
