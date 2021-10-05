c2fcf2e1a850d1481005b6926b9b32c4b4a25ec1
Sakirullahi <opshaktvl@gmail.com>
2021-10-05 15:53:06 +0530

    EditEntryModal compoenent added

---
+++ src/components/EditEntryModal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Modal } from 'semantic-ui-react'
+
+const EditEntryModal = ({isOpen}) => {
+    return (
+        <Modal open={isOpen}></Modal>
+    )
+}
+
+export default EditEntryModal
--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Fragment } from 'react/cjs/react.production.min'
 import { Grid, Icon, Segment } from 'semantic-ui-react'
+import EditEntryModal from './EditEntryModal'
 
 const EntryLine = ({id,isExpense=false,title, value, deleteEntry}) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
     return (
+      <Fragment>
         <Segment color={isExpense? 'red': 'green'}  >
         <Grid columns='3' divided textAlign='right'>
           <Grid.Row>
@@ -13,12 +19,14 @@ const EntryLine = ({id,isExpense=false,title, value, deleteEntry}) => {
               {value}
             </Grid.Column>
             <Grid.Column width='3'>
-              <Icon name='edit' bordered />
+              <Icon name='edit' bordered onClick={()=>setIsOpen(true)} />
               <Icon name='trash' bordered onClick={()=>deleteEntry(id)} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </Segment>
+      <EditEntryModal isOpen={isOpen} />
+    </Fragment>
 
     )
 }
