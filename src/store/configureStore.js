cedf441d1c0cb7744960358c687fb331d263900b
Sakirullahi <opshaktvl@gmail.com>
2021-10-06 17:29:30 +0530

    adding redux-devtools-extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,11 @@
 import { combineReducers, createStore } from "redux";
 import entriesReducer from "../reducers/entries.reducer";
+import {composeWithDevTools} from "redux-devtools-extension"
 
 const configureStore = ()=>{
       return createStore(combineReducers({
         entries:entriesReducer
-      }));
+      }),composeWithDevTools());
 }
 
 export default configureStore;
